8d0b4262b6ef8974ab084c7f3d3fcd2f32aab4b4
JoshReynolds <josh_reynolds_@hotmail.com>
2018-12-14 18:52:32 -0800

    updated comments and gitignore

--- src/config/diff.js
+++ src/config/diff.js
@@ -3,126 +3,122 @@ import render from "./render";
 // take in two arrays, return a 'zipped' array where it pushes an array with the
 // i index of each array. Loops through the shortest array.
 const zip = (arr1, arr2) => {
-  const zipped = [];
-  for (let i = 0; i < Math.min(arr1.length, arr2.length); i++) {
-    zipped.push([arr1[i], arr2[i]]);
-  }
-  return zipped;
+	const zipped = [];
+	for (let i = 0; i < Math.min(arr1.length, arr2.length); i++) {
+		zipped.push([arr1[i], arr2[i]]);
+	}
+	return zipped;
 };
 
 const diffAttrs = (oldAttrs, newAttrs) => {
-  const patches = [];
+	const patches = [];
 
-  // setting newAttrs
-  for (const [k, v] of Object.entries(newAttrs)) {
-    patches.push($node => {
-      $node.setAttribute(k, v);
-      return $node;
-    });
-  }
+	// setting newAttrs
+	for (const [k, v] of Object.entries(newAttrs)) {
+		patches.push($node => {
+			$node.setAttribute(k, v);
+			return $node;
+		});
+	}
 
-  // removing attrs if they don't exist in the newattrs
-  for (const k in oldAttrs) {
-    if (!(k in newAttrs)) {
-      patches.push($node => {
-        $node.removeAttribute(k);
-        return $node;
-      });
-    }
-  }
+	// removing attrs if they don't exist in the newattrs
+	for (const k in oldAttrs) {
+		if (!(k in newAttrs)) {
+			patches.push($node => {
+				$node.removeAttribute(k);
+				return $node;
+			});
+		}
+	}
 
-  return $node => {
-    for (const patch of patches) {
-      patch($node);
-    }
-    return $node;
-  };
+	// once called with $node, loop through all patches and return the updated node.
+	return $node => {
+		for (const patch of patches) {
+			patch($node);
+		}
+		return $node;
+	};
 };
 
 const diffChildren = (oldVChildren, newVChildren) => {
-  // go through all the old/new children up to the oldchildren length and diff them
-  const childPatches = [];
-  oldVChildren.forEach((oldVChild, i) => {
-    childPatches.push(diff(oldVChild, newVChildren[i]));
-  });
-  /**
-   
-   */
+	// go through all the old/new children up to the oldchildren length and diff them
+	const childPatches = [];
+	oldVChildren.forEach((oldVChild, i) => {
+		childPatches.push(diff(oldVChild, newVChildren[i]));
+	});
 
-  // if there are additional children past oldChildren length,
-  const additionalPatches = [];
-  for (const additionalVChild of newVChildren.slice(oldVChildren.length)) {
-    additionalPatches.push($node => {
-      $node.appendChild(render(additionalVChild));
-      return $node;
-    });
-  }
+	// if there are additional children past oldChildren length,
+	const additionalPatches = [];
+	for (const additionalVChild of newVChildren.slice(oldVChildren.length)) {
+		additionalPatches.push($node => {
+			$node.appendChild(render(additionalVChild));
+			return $node;
+		});
+	}
 
-  return $parent => {
-    // since childPatches are expecting the $child, not $parent, $parent childnodes gives nodelist of elements
-    // we cannot just loop through them and call patch($parent)
-    for (const [patch, $child] of zip(childPatches, $parent.childNodes)) {
-      patch($child);
-    }
+	return $parent => {
+		// since childPatches are expecting the $child, not $parent, $parent childnodes gives nodelist of elements
+		// we cannot just loop through them and call patch($parent)
+		for (const [patch, $child] of zip(childPatches, $parent.childNodes)) {
+			patch($child);
+		}
 
-    // for any remaining patches (zip func above ensures only new nodes are passed here) apply the patch
-    for (const patch of additionalPatches) {
-      patch($parent);
-    }
-    return $parent;
-  };
+		// for any remaining patches (zip func above ensures only new nodes are passed here) apply the patch
+		for (const patch of additionalPatches) {
+			patch($parent);
+		}
+		return $parent;
+	};
 };
 
 const diff = (oldVTree, newVTree) => {
-  // let's assume oldVTree is not undefined!
-  if (newVTree === undefined) {
-    return $node => {
-      $node.remove();
-      // the patch should return the new root node.
-      // since there is none in this case,
-      // we will just return undefined.
-      return undefined;
-    };
-  }
+	// let's assume oldVTree is not undefined!
+	if (newVTree === undefined) {
+		return $node => {
+			$node.remove();
+			// the patch should return the new root node.
+			// since there is none in this case,
+			// we will just return undefined.
+			return undefined;
+		};
+	}
 
-  if (typeof oldVTree === "string" || typeof newVTree === "string") {
-    if (oldVTree !== newVTree) {
-      // could be 2 cases:
-      // 1. both trees are string and they have different values
-      // 2. one of the trees is text node and
-      //    the other one is elem node
-      // Either case, we will just render(newVTree)!
-      return $node => {
-        const $newNode = render(newVTree);
-        $node.replaceWith($newNode);
-        return $newNode;
-      };
-    } else {
-      // this means that both trees are string
-      // and they have the same values
-      return $node => $node;
-    }
-  }
+	if (typeof oldVTree === "string" || typeof newVTree === "string") {
+		if (oldVTree !== newVTree) {
+			// could be 2 cases:
+			// 1. both trees are string and they have different values
+			// 2. one of the trees is text node and the other one is elem node
+			// Either case, we will just render(newVTree)!
+			return $node => {
+				const $newNode = render(newVTree);
+				$node.replaceWith($newNode);
+				return $newNode;
+			};
+		}
+		else {
+			// this means that both trees are string and they have the same values
+			return $node => $node;
+		}
+	}
 
-  if (oldVTree.tagName !== newVTree.tagName) {
-    // we assume that they are totally different and
-    // will not attempt to find the differences.
-    // simply render the newVTree and mount it.
-    return $node => {
-      const $newNode = render(newVTree);
-      $node.replaceWith($newNode);
-      return $newNode;
-    };
-  }
+	if (oldVTree.tagName !== newVTree.tagName) {
+		// we assume that they are totally different and will not attempt to find the differences.
+		// simply render the newVTree and mount it.
+		return $node => {
+			const $newNode = render(newVTree);
+			$node.replaceWith($newNode);
+			return $newNode;
+		};
+	}
 
-  const patchAttrs = diffAttrs(oldVTree.attrs, newVTree.attrs);
-  const patchChildren = diffChildren(oldVTree.children, newVTree.children);
+	const patchAttrs = diffAttrs(oldVTree.attrs, newVTree.attrs);
+	const patchChildren = diffChildren(oldVTree.children, newVTree.children);
 
-  return $node => {
-    patchAttrs($node);
-    patchChildren($node);
-    return $node;
-  };
+	return $node => {
+		patchAttrs($node);
+		patchChildren($node);
+		return $node;
+	};
 };
 
 export default diff;
